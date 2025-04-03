826b0644994121444e735a04d9e10c574b2012a4
ChrisHub8 <chrisboca41@gmail.com>
2025-04-03 14:38:01 -0700

    Code Journal #1 Session 2

---
+++ Session2.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+x = 1.2
+y = 3.2
+
+
+int1 = 2
+int2 = 5
+
+a_i = x+y
+
+b_i = int2 - int1
+
+c_i = y * int2
+
+
+print(a_i, type(a_i))
+print(b_i, type(b_i))
+print(c_i, type(c_i))
